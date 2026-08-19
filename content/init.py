cc449283cbfafe3259fe79e1f66b10f2f1f5ff72
Ruby <cherrymio48@gmail.com>
2026-08-20 00:47:56 +0200

    Load content and register events in init.py

--- content/init.py
+++ content/init.py
@@ -1,1 +1,10 @@
+from content.files import populate_filesystem
+from content.horror_events import register_all_events
+import content.custom_commands 
 
+def load_content(game):
+    if not game.state.flags.get("content_loaded"):
+        populate_filesystem(game.state.fs)
+        game.state.flags["content_loaded"] = True
+    
+    register_all_events(game)
